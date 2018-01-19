42e3b50edd8c44afd2fc547d05112505b705e438
Meron Tollosa <merontollosa@gmail.com>
2018-01-19 16:47:10 -0600

    update

--- Lab3/lab3.js
+++ Lab3/lab3.js
@@ -18,4 +18,33 @@ server.on('request',function (req, res) {
 });
 server.listen(8000 ,function(){console.log("Server Running");});
 
+//Lab  3 Answer !!!!!!!!!!!!!!!!!
+
+
+'use strict';
+class Clock extends EventEmitter{
+  constructor(){
+    super();
+    this.on('tick', () => {
+      console.log('TICK');
+  });
+ 
+}
+visit(){
+  setIntrval(1000, this.emit('tick'));
+}
+}
+
+var clock = new Clock();
+clock.visit();
+
+
+  
+
+
+
+
+
+
+
 
