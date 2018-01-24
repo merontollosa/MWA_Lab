893cf369d078158e76f000a10c380fb85e82250a
Meron Tollosa <merontollosa@gmail.com>
2018-01-23 20:07:45 -0600

    update

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -1,39 +1,27 @@
-var express = require('express');
-const bodyParser = require('body-parser');
-const crypto = require('crypto'),
-     algorithm = 'aes-256',
-     password = 'asaadsaad';
-
-
-
-var app= express();
-var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/homework7", {native_parser:true});
-var encrypted ="";
-db.collection('message').findOne({},function(err, result) {
-   console.dir(result.message);
-   encrypted=result.message;
-        db.close();
+const express = require('express');
+      bodyParser = require('body-parser'),
+      crypto = require('crypto'),
+      algorithm = 'aes-256',
+      password = 'asaadsaad',
+      app= express(),
+      mongo = require('mongoskin'),
+      db = mongo.db("mongodb://localhost:27017/homework7", {native_parser:true}),
+      encrypted ="",
+      db.collection('message').findOne({},function(err, result) {
+           console.dir(result.message);
+           encrypted=result.message;
+           db.close();
 });
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
- 
  app.get('/', function(req, res){
-         
-           // db.collection('message').findOne()(function(err, items) {
-               // find();
-               // console.log(items);
              res.send("Decrypted Value :" +decrypt());
             });
         
-   
-
  app.listen(8000, () => {console.log("Server Started....")});
  
-
  
 // Prints: some clear text data
 function decrypt(){
